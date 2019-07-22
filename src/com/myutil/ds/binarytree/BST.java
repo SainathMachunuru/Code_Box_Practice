20651a369d6a2ce271b1f7a2d9ce6f586ae81e2f
Sainath Machunur <Sainath.Machunur1@T-Mobile.com>
2019-07-22 17:55:47 +0530

    Binary Search Tree DS practice

--- src/com/myutil/ds/binarytree/BST.java
+++ src/com/myutil/ds/binarytree/BST.java
@@ -1,5 +1,18 @@
 package com.myutil.ds.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * The depth of a node is the number of edges from the root to the node. The
+ * height of a node is the number of edges from the node to the deepest leaf.
+ * The height of a tree is a height of the root. A full binary tree.is a binary
+ * tree in which each node has exactly zero or two children. A complete binary
+ * tree is a binary tree, which is completely filled, with the possible
+ * exception of the bottom level, which is filled from left to right. Bread
+ * first traversal: Level Order traversal Depth first traversal:pre order,in
+ * order,post order
+ */
 public class BST {
 	Node root = null;
 
@@ -38,6 +51,202 @@ public class BST {
 			prinTree(node.right);
 		}
 
+	}
+	// recursive approach
+		public int getMinElement(Node root) {
+			if (root == null) {
+				return -1;
+			} else if (root.left == null) {
+				return root.data;
+			} else {
+				return getMinElement(root.left);
+			}
+
+		}
+
+		// iterative approach
+		public int getMaxElement(Node root) {
+			if (root == null) {
+				return -1;
+			}
+			while (root.right != null) {
+				root = root.right;
+			}
+			return root.data;
+
+		}
+
+		public int height(Node root) {
+			if (root == null) {
+				return -1;
+			}
+			int leftHeight = height(root.left);
+			int rightHeight = height(root.right);
+
+			return Math.max(leftHeight, rightHeight) + 1;
+
+		}
+
+		public void levelOrder(Node root) {
+			Queue<Node> queue = new LinkedList<Node>();
+			if (root == null) {
+				return;
+
+			} else {
+				queue.add(root);
+			}
+			while (!queue.isEmpty()) {
+				Node current = queue.remove();
+				System.out.print(current.data + " ");
+				if (current.left != null) {
+					queue.add(current.left);
+
+				}
+				if (current.right != null) {
+					queue.add(current.right);
+
+				}
+
+			}
+			System.out.println();
+
+		}
+
+		public void preOrder(Node root) {
+			if (root == null) {
+				return;
+			}
+			System.out.print(root.data + " ");
+			preOrder(root.left);
+			preOrder(root.right);
+
+		}
+
+		public void postOrder(Node root) {
+			if (root == null) {
+				return;
+			}
+			postOrder(root.left);
+			postOrder(root.right);
+			System.out.print(root.data + " ");
+
+		}
+
+		public void inOrder(Node root) {
+			if (root == null) {
+				return;
+			}
+			inOrder(root.left);
+			System.out.print(root.data + " ");
+			inOrder(root.right);
+		}
+		
+		public boolean isBST(Node root, int minVal, int maxVal) {
+			if (root == null) {
+				return true;
+
+			}
+			if (root.data >= minVal && root.data <= maxVal && isBST(root.left, minVal, root.data)
+					&& isBST(root.right, root.data, maxVal)) {
+				return true;
+
+			} else {
+				return false;
+			}
+		}
+
+	public Node delete(Node root,int data) {
+		if(root == null) {
+			return root;
+			
+		}
+		else if(data<root.data) {
+			root.left = delete(root.left, data);
+			
+		}
+		else if(data>root.data) {
+			root.right = delete(root.right, data);
+		}
+		else {
+			//case 1 no child
+			if(root.left == null && root.right == null) {
+				root = null;
+				
+			}
+			//case 2 one child
+			else if(root.left == null) {
+				Node temp = root;
+				root = root.right;
+				temp = null;
+				
+			}
+			else if(root.left == null) {
+				Node temp = root;
+				root = root.left;
+				temp = null;
+				
+			}
+			//case 3 two child 
+			else {
+				Node temp = getMinNode(root.right);
+				root.data = temp.data;
+				root.right = delete(root.right, data);
+				
+			}
+		}
+		return root;
+		
+		
+	}
+	public Node getInorderSuccessor(Node root,int data) {
+		Node current = searchNode(root,data);
+		if(current == null) {
+			return null;
+		}
+		if(current.right!=null) { //case1 node has  right subtree
+			return getMinNode(current.right);
+		}
+		else { //case2 no right subtree
+			Node successor = null;
+			Node ancestor = root;
+			while(ancestor!=null) {
+				if(current.data<ancestor.data) {
+					successor = ancestor;
+					ancestor = ancestor.left;
+					
+				}
+				else {
+					ancestor = ancestor.right;
+				}
+				
+			}
+			return successor;
+			
+		}
+	}
+
+	private Node searchNode(Node root,int data) {
+		if (root == null) {
+			return root;
+		} else if (data == root.data) {
+			return root;
+		} else if (data <= root.data) {
+			return searchNode(root.left, data);
+
+		} else {
+			return searchNode(root.right, data);
+
+		}
+	}
+
+	private Node getMinNode(Node root) {
+		if (root == null) {
+			return root;
+		} else if (root.left == null) {
+			return root;
+		} else {
+			return getMinNode(root.left);
+		}
 	}
 
 }
--- src/com/myutil/ds/binarytree/BSTMain.java
+++ src/com/myutil/ds/binarytree/BSTMain.java
@@ -5,27 +5,71 @@ public class BSTMain {
 	public static void main(String[] args) {
 		BST bst = new BST();
 		Node node = bst.root;
-		node = bst.insert(node,6);
-		node = bst.insert(node,3);
-		node = bst.insert(node,7);
-		node =bst.insert(node,1);
-		node =bst.insert(node,2);
-		node = bst.insert(node,8);
-		node = bst.insert(node,9);
-		
-		int searchElement = 7;
-		
-		System.out.println("**********Elements of BST :: INORDER**********");
-		bst.prinTree(node);
-		System.out.println("**********************************************");
-		
-		System.out.println("**********Serching for element "+searchElement+" . . . . .");
-		System.out.println(bst.search(node, 5)==true?"Element Found":"Element not found");
-		System.out.println("*********************************************************");
-		
-		
+		node = bst.insert(node, 6);
+		node = bst.insert(node, 3);
+		node = bst.insert(node, 7);
+		node = bst.insert(node, 1);
+		node = bst.insert(node, 0);
+		node = bst.insert(node, 2);
+		node = bst.insert(node, 8);
+		node = bst.insert(node, 9);
 		
 
+		int searchElement = 7;
+		int searchSuccessor = 0;
+
+		System.out.println("**********Elements of BST :: INORDER**********");
+		bst.inOrder(node);
+		System.out.println("**********************************************");
+
+		System.out.println("**********Serching for element " + searchElement + " . . . . .");
+		System.out.println(bst.search(node, 5) == true ? "Element Found" : "Element not found");
+		System.out.println("*********************************************************");
+
+		System.out.println("**********The Minimum Elelement of given BST is: **********");
+		System.out.println(bst.getMinElement(node) == -1 ? "Error :: tree is empty" : bst.getMinElement(node));
+		System.out.println("**********************************************");
+
+		System.out.println("**********The Maximum Elelement of given BST is: **********");
+		System.out.println(bst.getMinElement(node) == -1 ? "Error :: tree is empty" : bst.getMaxElement(node));
+		System.out.println("**********************************************");
+
+		System.out.println("**********Height(Max Depth) of BST**********");
+		System.out.println(bst.height(node));
+		System.out.println("**********************************************");
+		
+		System.out.println("**********Level order traversal(breadth first) of BST**********");
+		bst.levelOrder(node);
+		System.out.println("**********************************************");
+		
+		System.out.println("********** Pre-Order traversal(depth first) of BST**********");
+		bst.preOrder(node);
+		System.out.println("\n**********************************************");
+		
+		System.out.println("********** Post-Order traversal(depth first) of BST**********");
+		bst.postOrder(node);
+		System.out.println("\n**********************************************");
+		
+		System.out.println("********** In-Order traversal(depth first) of BST**********");
+		bst.inOrder(node);
+		System.out.println("\n**********************************************");
+		
+		System.out.println("********** Is given BST is balanced ??? **********");
+		System.out.println(bst.isBST(node, Integer.MIN_VALUE, Integer.MAX_VALUE));
+		System.out.println("\n**********************************************");
+		
+		/*System.out.println("**********Deleting a node from BST**********");
+		bst.delete(node,8);
+		System.out.println("**********************************************");
+		
+		System.out.println("********** Elements of deleteing node of BST**********");
+		bst.preOrder(node);
+		System.out.println("\n**********************************************");*/
+		
+		System.out.println("**********Inorder Succcessor of Node: "+searchSuccessor+" **********");
+		System.out.println(bst.getInorderSuccessor(node, searchSuccessor).data);
+		System.out.println("**********************************************");
+		
 	}
 
 }
