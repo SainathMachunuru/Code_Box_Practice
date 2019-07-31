cf81de5ea3f8d2e747112b3722dbbc3422748e53
Sainath2236 <machunursainathgoud@gmail.com>
2019-07-31 22:44:31 +0530

    bubble sort alogoritham

--- src/com/myutil/algo/sorting/SortingUtil.java
+++ src/com/myutil/algo/sorting/SortingUtil.java
@@ -23,6 +23,32 @@ public class SortingUtil {
 		}
 
 	}
+	
+	/**
+	 * 
+	 * In-Place sorting algoritham,Time complexity O(n^2)
+	 */
+	public void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			int falg = 0;
+			for (int j = 0; j < n - i - 1; j++) {
+				if(arr[j]>arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					falg = 1;	
+				}
+
+			}
+			if(falg == 0){
+				break;
+			}
+			print(arr, i);
+
+		}
+
+	}
 
 	public void print(int arr[], int pass) {
 		System.out.print("pass " + (pass + 1) + ":: ");
