1661ad3ebe8a913f70459cb7a8bb7ea6b7465538
Sainath2236 <machunursainathgoud@gmail.com>
2019-08-03 16:38:42 +0530

    Arraay practice

--- src/com/myutil/ds/array/ArrayCodeBox.java
+++ src/com/myutil/ds/array/ArrayCodeBox.java
@@ -3,6 +3,8 @@ package com.myutil.ds.array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ArrayCodeBox {
@@ -174,5 +176,23 @@ public class ArrayCodeBox {
 		}
 
 	}
+	/** Given an array of integers, 
+	 * return indices of the two numbers 
+	 * such that they add up to a specific target. 
+	 */
+	public int[] twoSum(int nums[], int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[] { i, map.get(complement) };
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+	
 
 }
