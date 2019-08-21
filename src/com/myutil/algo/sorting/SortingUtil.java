0c65d06915072f2f85fdbf110ce27d99b9b861e2
Sainath Machunur <Sainath.Machunur1@T-Mobile.com>
2019-08-21 15:17:15 +0530

    merge sort implementation

--- src/com/myutil/algo/sorting/SortingUtil.java
+++ src/com/myutil/algo/sorting/SortingUtil.java
@@ -23,7 +23,7 @@ public class SortingUtil {
 		}
 
 	}
-	
+
 	/**
 	 * 
 	 * In-Place sorting algoritham,Time complexity O(n^2)
@@ -33,15 +33,15 @@ public class SortingUtil {
 		for (int i = 0; i < n; i++) {
 			int falg = 0;
 			for (int j = 0; j < n - i - 1; j++) {
-				if(arr[j]>arr[j+1]){
+				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-					falg = 1;	
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					falg = 1;
 				}
 
 			}
-			if(falg == 0){
+			if (falg == 0) {
 				break;
 			}
 			print(arr, i);
@@ -50,8 +50,95 @@ public class SortingUtil {
 
 	}
 
+	/**
+	 * 
+	 * In-Place sorting algoritham,Time complexity O(n^2)
+	 */
+	public void insertionSort(int arr[]) {
+		for (int i = 1; i < arr.length; i++) {
+			int j = i;
+			int temp = arr[j];
+			while (j > 0 && arr[j - 1] > temp) {
+				arr[j] = arr[j - 1];
+				j--;
+
+			}
+			arr[j] = temp;
+			print(arr, i - 1);
+
+		}
+
+	}
+	/**
+	 * Divide and conquer , recursive approach
+	 * Stable sort:	maintain the order of elements with same value in orginal array
+	 * Not in-place sort : need extra memory
+	 * Space complexity O(n)
+	 * Time complexity O(nlogn)
+	 * 
+	 */
+	public void mergeSort(int arr[]) {
+		int n = arr.length;
+		if (n < 2) {
+			return;
+		}
+		int mid = n / 2;
+		int[] left = new int[mid];
+		int[] right = new int[n - mid];
+		for (int i = 0; i < mid; i++) {
+			left[i] = arr[i];
+		}
+		for (int i = mid; i < n; i++) {
+			right[i - mid] = arr[i];
+		}
+		mergeSort(left);
+		mergeSort(right);
+		merge(arr, left, right);
+		print(arr);
+
+	}
+
+	private void merge(int[] arr, int[] left, int[] right) {
+		int i = 0, j = 0, k = 0;
+		while (i < left.length && j < right.length) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				k++;
+				i++;
+
+			} else {
+				arr[k] = right[j];
+				k++;
+				j++;
+
+			}
+
+		}
+		while (i < left.length) {
+			arr[k] = left[i];
+			k++;
+			i++;
+
+		}
+		while (j < right.length) {
+			arr[k] = right[j];
+			k++;
+			j++;
+
+		}
+
+	}
+
 	public void print(int arr[], int pass) {
 		System.out.print("pass " + (pass + 1) + ":: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+	public void print(int arr[]) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
--- src/com/myutil/algo/sorting/SortingUtilMain.java
+++ src/com/myutil/algo/sorting/SortingUtilMain.java
@@ -2,9 +2,11 @@ package com.myutil.algo.sorting;
 
 public class SortingUtilMain {
 	public static void main(String[] args) {
-		int arr[] = {2,5,1,6,3,7};
+		int arr[] = {2,4,1,6,8,5,3,7};
 		//new SortingUtil().selectionSort(arr);
-		new SortingUtil().bubbleSort(arr);
+		//new SortingUtil().bubbleSort(arr);
+		//new SortingUtil().insertionSort(arr);
+		new SortingUtil().mergeSort(arr);
 	}
 
 }
